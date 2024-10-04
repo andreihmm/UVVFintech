b9bd6601c9493868884a6708f1efd989992d06f5
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2024-10-04 10:19:31 -0300

    Add files via upload

---
+++ persistencia/BD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UVVFintech.model;
+
+namespace UVVFintech.persistencia
+{
+    internal class BD
+    {
+        private List<Cliente> clientes;
+
+        public void salvarBD(Cliente cliente)
+        {
+
+        }
+
+        public List<Cliente> retornarBD()
+        {
+            return clientes;
+        }
+
+        public Cliente retornarBD(String CPF)
+        {
+            return clientes
+        }
+
+        public void removerBD(String CPF) {
+        
+        }
+
+        public bool existe(String CPF) {
+            return false;
+        }
+    }
+}
